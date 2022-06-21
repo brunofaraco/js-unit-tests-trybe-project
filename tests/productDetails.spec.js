900146c72b13b4db41635246240fec62356925dc
Bruno <brunocfaraco@hotmail.com.br>
2022-06-21 18:38:59 -0300

    implementação dos its nos arquivos de teste feitos por causa de requisitos

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,23 +30,24 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    // fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    expect(typeof productDetails).toEqual('function');
-    // Teste se o retorno da função é um array.
+  describe('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+     
+    it('Teste se productDetails é uma função.', () => {expect(typeof productDetails).toEqual('function');});
+    
+    it('Teste se o retorno da função é um array.', () => {
     expect(productDetails('abacate', 'laranja')).toBeInstanceOf(Array);
-    expect(Array.isArray(productDetails('abacate', 'laranja'))).toBeTruthy();
-    // Teste se o array retornado pela função contém dois itens dentro.
-    expect(Object.keys(productDetails('Copo', 'Saco de lixo')).length).toEqual(2);
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(Array.isArray(productDetails('abacate', 'laranja'))).toBeTruthy();});
+
+    it('Teste se o array retornado pela função contém dois itens dentro.', () => {expect(Object.keys(productDetails('Copo', 'Saco de lixo')).length).toEqual(2);});
+
+    it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
     expect(typeof Object.keys(productDetails('Vodka', 'Energético')[0])).toEqual('object');
-    expect(typeof Object.keys(productDetails('Vodka', 'Energético')[1])).toEqual('object'); 
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    expect(productDetails('Prato', 'Garfo')[0]).not.toEqual(productDetails('Prato', 'Garfo')[1]);
-    // Teste se os dois productIds terminam com 123.
+    expect(typeof Object.keys(productDetails('Vodka', 'Energético')[1])).toEqual('object'); });
+
+    it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {expect(productDetails('Prato', 'Garfo')[0]).not.toEqual(productDetails('Prato', 'Garfo')[1]);});
+
+    it('Teste se os dois productIds terminam com 123.', () => {
     expect(productDetails('Webcam', 'Microfone')[0].details.productId.endsWith('123')).toBeTruthy();
-    expect(productDetails('Webcam', 'Microfone')[1].details.productId.endsWith('123')).toBeTruthy();
+    expect(productDetails('Webcam', 'Microfone')[1].details.productId.endsWith('123')).toBeTruthy();});
   });
 });
