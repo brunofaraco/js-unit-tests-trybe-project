9a64caa37fc69617ba379134e305b74c0fb3f6f8
Bruno <brunocfaraco@hotmail.com.br>
2023-04-26 16:36:08 -0300

    feat: finished implementation of funciont "createMenu"

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,68 +1,28 @@
 /* eslint-disable max-len */
 
-/*
-  Você é responsável por escrever o código do sistema de pedidos de um restaurante através do qual será possível
-  cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto que permite:
-
-  - Ler o menu cadastrado;
-  - Fazer pedidos;
-  - Verificar o que foi pedido;
-  - Somar o valor da conta.
-
-  A estrutura deste código e deste objeto já está definida e você precisa implementá-la.
-  Abaixo você verá uma série de testes e passos que irão guiar você e, que devem NECESSARIAMENTE ser realizados em ordem para o bom desenvolvimento do sistema.
-
-  Desenvolvimento:
-  - Uma função:
-    createMenu()
-  - Recebe um parâmetro que é um objeto, o menu:
-    Exemplo: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }.
-
-  A função createMenu() então, retornará um novo objeto. Este novo objeto contém algumas chaves e ao acessar cada uma delas temos os seguintes retornos:
-
-  - Uma chave `fetchMenu` retornando o menu, que nada mais é que o objeto passado como parâmetro para createMenu()
-
-    Exemplo:
-    const meuRestaurante = createMenu({
-      food: {'coxinha': 3.90, 'sanduiche', 9.90},
-      drinks: {'agua': 3.90, 'cerveja': 6.90}
-    });
-
-    meuRestaurante.fetchMenu() // Retorno: Menu acima
-
-  - Uma chave `consumption` armazenando um array de strings. Cada string é a chave de um pedido.
-    Exemplo: ['coxinha', 'cerveja']
-
-  - Uma chave `order` armazenando uma função. Essa função recebe uma string como parâmetro e essa string deve ser adicionada à lista armazenada em `consumption`.
-
-  - Uma chave `pay` que, quando chamada, invoca uma função. Essa função faz a soma do preço de todos os pedidos, retornando essa soma de preços com acréscimo de 10%.
-
-  Comportamento:
-    const meuRestaurante = createMenu({ food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} })
-
-    meuRestaurante.fetchMenu() // Retorno: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }
-
-    meuRestaurante.order('coxinha') // Retorno: undefined
-
-    meuRestaurante.consumption // Retorno: ['coxinha']
-
-    meuRestaurante.pay() // Retorno: 4.29
-
-  IMPORTANTE - FAÇA OS TESTES E PASSOS DE ACORDO COM A SEQUÊNCIA INDICADA NO README DO PROJETO!
-
-  BOAS PRÁTICAS TDD: COMECE PELO TESTE 1 DO ARQUIVO `tests/restaurant.spec.js` E VOLTE A ESTE ARQUIVO QUANDO FOR INDICADO!
-
-*/
-
-// PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` armazenando uma função
-// que:
-// - percorrerá item a item de `objetoRetornado.consumption`;
-// - fará a soma do preço desses itens;
-// - retornará o valor somado acrescido de 10%.
-// DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
-
 function order(str) {
   this.consumption.push(str)
+}
+
+function pay() {
+  const menuObj = this.fetchMenu()
+  const consumptionArr = this.consumption
+
+  let foodsTotalPrice = 0
+
+  if (consumptionArr.length) {
+    consumptionArr.forEach((e) => {
+      if (e in menuObj.food){
+        foodsTotalPrice += menuObj.food[e]
+      } else {
+        foodsTotalPrice += menuObj.drink[e]
+      }
+    })
+
+    const finalPrice = (foodsTotalPrice + (foodsTotalPrice * 0.1)).toFixed(1)
+
+    return finalPrice
+  }
 }
 
 const createMenu = (menuObj) => {
@@ -72,6 +32,7 @@ const createMenu = (menuObj) => {
     fetchMenu,
     consumption: [],
     order,
+    pay,
   }
 
 
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,58 +1,6 @@
 const createMenu = require('../src/restaurant');
- 
-/*
-  Você é responsável por escrever o código do sistema de pedidos de um restaurante através do qual será possível
-  cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto que permite:
 
-  - Ler o menu cadastrado;
-  - Fazer pedidos;
-  - Verificar o que foi pedido;
-  - Somar o valor da conta.
-
-  A estrutura deste código e deste objeto já está definida e você precisa implementá-la.
-  Abaixo você verá uma série de testes e passos que irão guiar você e, que devem NECESSARIAMENTE ser realizados em ordem para o bom desenvolvimento do sistema.
-
-  Desenvolvimento:
-  - Uma função: 
-    createMenu()
-  - Recebe um parâmetro que é um objeto, o menu:
-    Exemplo: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }.
-
-  A função createMenu() então, retornará um novo objeto. Este novo objeto contém algumas chaves e ao acessar cada uma delas temos os seguintes retornos:
-
-  - Uma chave "fetchMenu" retornando o menu, que nada mais é que o objeto passado como parâmetro para createMenu()
-
-    Exemplo:
-    const meuRestaurante = createMenu({
-      food: {'coxinha': 3.90, 'sanduiche', 9.90},
-      drinks: {'agua': 3.90, 'cerveja': 6.90}
-    });
-
-    meuRestaurante.fetchMenu() // Retorno: Menu acima
-
-  - Uma chave "consumption" armazenando um array de strings. Cada string é a chave de um pedido.
-    Exemplo: ['coxinha', 'cerveja']
-
-  - Uma chave "order" armazenando uma função. Essa função recebe uma string como parâmetro e essa string deve ser adicionada à lista armazenada em "consumption".
-
-  - Uma chave "pay" que, quando chamada, invoca uma função. Essa função faz a soma do preço de todos os pedidos, retornando essa soma de preços com acréscimo de 10%.
-
-  Comportamento:
-    const meuRestaurante = createMenu({ food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} })
-
-    meuRestaurante.fetchMenu() // Retorno: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }
-
-    meuRestaurante.order('coxinha') // Retorno: undefined
-
-    meuRestaurante.consumption // Retorno: ['coxinha']
-
-    meuRestaurante.pay() // Retorno: 4.29
-
-  IMPORTANTE: FAÇA OS TESTES E PASSOS DE ACORDO COM A SEQUÊNCIA INDICADA NO README DO PROJETO!
-
-*/
-
-describe.only('10 - Implemente os casos de teste e a função "createMenu"', () => {
+describe('10 - Implemente os casos de teste e a função "createMenu"', () => {
   describe('Verifica se a função "createMenu" tem o comportamento esperado', () => {
     it('TESTE 1: Verifique se função "createMenu()" retorna um objeto que possui a chave "fetchMenu", a qual tem como valor uma função.', () => {
     const data = createMenu()
@@ -115,16 +63,25 @@ describe.only('10 - Implemente os casos de teste e a função "createMenu"', () 
     })
 
     it('TESTE 8: Verifique se, ao chamar "objetoRetornado.pay()", retorna-se a soma dos preços de tudo que foi pedido, conforme registrado em "objetoRetornado.consumption"', () => {
-      const data = createMenu()
+      const paramMock = { 
+        food: {
+          "coxinha": 2.9,
+          "brigadeiro": 1.5,
+        },
+        drink: {
+          "coca-cola": 3.9,
+        } };
+
+      const data = createMenu(paramMock)
       
       data.order("coxinha") // price 2.9
       data.order("coca-cola") // price 3.9
       data.order("brigadeiro") // price 1.5
 
       const sum = 2.9 + 3.9 + 1.5;
-      // const expectedPrice =  sum + Math.floor((sum * 0.1))
+      const expectedPrice =  (sum + (sum * 0.1)).toFixed(1)
 
-      expect(data.pay()).toBe(sum)
+      expect(data.pay()).toBe(expectedPrice)
     })
   });
 });
