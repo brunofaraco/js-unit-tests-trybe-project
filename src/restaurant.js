7173d241863c120d329bc0bba43666c9d70315ec
Bruno <brunocfaraco@hotmail.com.br>
2023-04-26 16:39:53 -0300

    fix: fixed lint errors

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,42 +1,41 @@
 /* eslint-disable max-len */
 
 function order(str) {
-  this.consumption.push(str)
+  this.consumption.push(str);
 }
 
 function pay() {
-  const menuObj = this.fetchMenu()
-  const consumptionArr = this.consumption
+  const menuObj = this.fetchMenu();
+  const consumptionArr = this.consumption;
 
-  let foodsTotalPrice = 0
+  let foodsTotalPrice = 0;
 
   if (consumptionArr.length) {
     consumptionArr.forEach((e) => {
-      if (e in menuObj.food){
-        foodsTotalPrice += menuObj.food[e]
+      if (e in menuObj.food) {
+        foodsTotalPrice += menuObj.food[e];
       } else {
-        foodsTotalPrice += menuObj.drink[e]
+        foodsTotalPrice += menuObj.drink[e];
       }
-    })
+    });
 
-    const finalPrice = (foodsTotalPrice + (foodsTotalPrice * 0.1)).toFixed(1)
+    const finalPrice = (foodsTotalPrice + (foodsTotalPrice * 0.1)).toFixed(1);
 
-    return finalPrice
+    return finalPrice;
   }
 }
 
 const createMenu = (menuObj) => {
-  const fetchMenu = () => menuObj
+  const fetchMenu = () => menuObj;
   
   const menu = {
     fetchMenu,
     consumption: [],
     order,
     pay,
-  }
+  };
 
-
-  return menu
+  return menu;
 };
 
 module.exports = createMenu;
